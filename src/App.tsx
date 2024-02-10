f74333bedaa4e2524cd0cf79c8ada53dcb792306
julia-lahtina <j.lakhtin@gmail.com>
2024-02-10 11:13:39 +0100

    add array

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import './App.css';
 import {TodoList} from "./components/TodoList";
 
 function App() {
+
+    const tasks = [
+        {id:1, title: "HTML&CSS", isDone: true},
+        {id:2, title: "JS", isDone: true},
+        {id:3, title: "React", isDone: false},
+    ]
+
     return (
         <div className="App">
             <TodoList/>
