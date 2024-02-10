a15f8df8b94daf21d369f90fb559678921f3527b
julia-lahtina <j.lakhtin@gmail.com>
2024-02-10 10:37:56 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <h3>What to learn</h3>
+            <div>
+                <input type="text"/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked/> <span>JS</span></li>
+                <li><input type="checkbox"/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
