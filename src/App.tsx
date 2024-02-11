a1c239df480b786932806f6b0d5b80a93678563e
julia-lahtina <j.lakhtin@gmail.com>
2024-02-11 12:14:48 +0100

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./components/TodoList";
 
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "Rest API", isDone: false},
         {id: 5, title: "GraphQL", isDone: false},
-    ]
+    ])
 
     function removeTask(id: number) {
         tasks = tasks.filter(task => task.id !== id)
-        console.log(tasks)
+        setTasks(tasks)
     }
 
     return (
