7fdd3f483b45b9c75f9a6b494b0ecfc17a2108dc
julia-lahtina <j.lakhtin@gmail.com>
2024-02-23 16:44:24 +0100

    add onClick

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {BooksType} from '../App';
 
-export const Book = ({books}: BooksType) => {
+export const Book = ({books, deleteBook}: BooksType) => {
     return (
         <ul>
             {books.map(b => {
+
+                const handleChangeDelete = () => {
+                    deleteBook(b.id)
+                }
+
                 return (
                     <li key={b.id}>
                         <input checked={b.isRead} type="checkbox"/>
                         <span>{b.bookName}</span>
-                        <span className={'delete-book'}><em>  ...удалить из списка</em></span>
+                        <span onClick={handleChangeDelete}
+                              className={'delete-book'}>
+                            <em>  ...удалить из списка</em>
+                        </span>
                     </li>
                 )
             })}
