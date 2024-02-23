c67e5afbc1d70b2ff64c17580c7dd32c342a683c
julia-lahtina <j.lakhtin@gmail.com>
2024-02-23 16:44:07 +0100

    add new props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,22 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, KeyboardEvent} from 'react';
 
 export type InputPropsType = {
     inputType: string
     newValue: string
     setNewInputValue: (newValue: string) => void
+    onKeyPress: (e: KeyboardEvent<HTMLInputElement>) => void
 }
-export const Input = ({setNewInputValue, newValue, inputType}: InputPropsType) => {
+export const Input = ({setNewInputValue, newValue, inputType, onKeyPress}: InputPropsType) => {
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setNewInputValue(e.currentTarget.value)
     }
-    console.log(newValue)
+
+    const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyPress(e)
+    }
+
     return (
-        <input value={newValue} onChange={handleChange} type={inputType}/>
+        <input value={newValue} onChange={handleChange} onKeyDown={handleOnKeyDown} type={inputType}/>
     );
 };
